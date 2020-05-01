0d6b3648d24f3691a4cf99e9da917aa8bf7ed2bd
kihun0227@gmail.com <kihun0227@gmail.com>
2020-05-01 15:23:32 +0900

    init commit

---
+++ src/Test/MultiServer.java
@@ -0,0 +1,59 @@
+package Test;
+
+import java.io.*;
+import java.net.*;
+
+public class MultiServer {
+
+	public static void main(String[] args) {
+
+		ServerSocket serverSocket = null;
+		Socket socket = null;
+		PrintWriter out = null;
+		BufferedReader in = null;
+		String s = "";
+		String name = "";
+
+		try {
+			serverSocket = new ServerSocket(9999);
+			System.out.println("서버가 시작되었습니다.");
+
+			socket = serverSocket.accept();
+
+			out = new PrintWriter(socket.getOutputStream(), true);
+			in = new BufferedReader(new
+					InputStreamReader(socket.getInputStream()));
+
+			if(in != null) {
+				name = in.readLine();
+				System.out.println(name +" 접속");
+				out.println("> "+ name +"님이 접속했습니다.");
+			}
+
+			while(in != null) {
+				s = in.readLine();
+				if(s==null) {
+					break;
+				}
+				System.out.println(name +" ==> "+ s);
+				out.println(">  "+ name +" ==> "+ s);
+			}
+
+			System.out.println("Bye...!!!");
+		}
+		catch (Exception e) {
+			System.out.println("예외1:"+ e);
+		}
+		finally {
+			try {
+				in.close();
+				out.close();
+				socket.close();
+				serverSocket.close();
+			}
+			catch (Exception e) {
+				System.out.println("예외2:"+ e);
+			}
+		}
+	}
+}
